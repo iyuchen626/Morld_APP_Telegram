77465c34ac583a8f0aba568a6a761aebad79ff8f
iyuchen626 <cheniyu840626@gmail.com>
2023-02-06 04:50:00 +0800

    Pose Detection & move to earn
    Add Pose Detection & MOVE to earn

---
+++ MorldApp_Demo01/app/src/main/java/com/example/morldapp_demo01/fragmemt/VideoFragment.java
@@ -0,0 +1,28 @@
+package com.example.morldapp_demo01.fragmemt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.morldapp_demo01.R;
+import com.example.morldapp_demo01.camera.VideoRecordingActivity;
+
+
+public class VideoFragment extends Fragment {
+    private View mview;
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mview=inflater.inflate(R.layout.fragment_video,container,false);
+        Intent intent = new Intent();
+        intent= new Intent(this.getActivity(), VideoRecordingActivity.class);
+        startActivity(intent);
+        return mview;
+    }
+}
